3d9131e0e4390d0aa1e9736f08a599a05f6898fd
xiejianming <xiejianming@gmail.com>
2013-03-20 01:10:08 +0800

    fill the Keng

--- README.md
+++ README.md
@@ -1,10 +1,75 @@
 # dbkid
 
-Simple debugging facilities. 
+Simple debugging facilities.
+
+## Note
+### Not-A-Real-Debugger
+This is not really a debugger, so it cannot use to step through code executions
+### Only-To-Capture-Running-Values
+This simple took kit is only a convenient way to capture values when running code.
+### Has-Limits
+This is not a perfect tool - it has its own limits. E.g. cannot capture binding Vars. 
+### Let-Me-No....
+Please let me(xiejianming@gmail.com) know if you have anything (or bug report) to say....
 
 ## Usage
+There are two tools to help investigate values during executing: 'dbk' & '?', and their own switch: 'db'(debug) & 'bp'(break-point).
 
-(TBD)
+### 1 use dbkid.core
+E.g. 
+=========================
+(use 'dbkid.core)
+=========================
+### 2 use 'dbk' - a convenient printing function
+E.g.
+=========================
+(let [x 123 y (range 10)]
+   (dbk x y (map #(* % %) y))
+   "Done!")
+=========================
+then we will get:
+Debug: 2013-03-20 00:42:47 in NO_SOURCE_PATH@2: {x => 123, y => (0 1 2 3 4 5 6 7 ...), (map (fn* [p1__1420#] (* p1__1420# p1__1420#)) y) => (0 1 4 9 16 25 36 49 ...)}
+"Done!"
+#### 2.1 turn off dbk
+=========================
+(db)
+(let [x 123 y (range 10)]
+     (dbk x y (map #(* % %) y))
+     "Done!")
+=========================
+and now we get:
+DBK is now Disabled!
+nil
+"Done!"
+#### 2.2 turn on dbk
+just "(db)" it again....
+
+### 3 use '?' - an interactive "break-point"
+Try following code:
+=========================
+(def ^:dynamic xx 111111)
+(binding [xx "this is xx"]
+  (let [a 123 b (defn tt[] (println a)) e [1 tt 3 (println 999)]]
+    (let [a 789 c "wtf" d (range 11) f {1 2 3 (range 3) 4 (rand)} 
+          g (fn [] (println "ggggggg")) h (def xxx 222222)]
+      (println a) (println b) (println c)
+      (println d) (println e) (println f)
+      (println g) (println h)
+      (? println xx))))
+=========================
+
+Note: if above code doesn't surprise you, please turn on '?' and then try them again.
+
+#### 3.1 turn on/off break-point
+Use "(bp)" to turn on/off break-point
+#### 3.2 bp instructions
+When you see a prompt, following instructions make bp work its way:
+1) '?' - print the current line info of break-point 
+2) 'l' - list locals
+3) 'll' - list 'global' Vars
+4) local and var's name - print the value of given local/var
+5) normal clojure form - eval the given form
+6) 'q' or ESC or ENTER - get through current break point
 
 ## License
 
--- src/dbkid/core.clj
+++ src/dbkid/core.clj
@@ -1,2 +1,84 @@
+; Copyright (c) xjm (xiejianming@gmail.com). All rights reserved.
+; Distributed under the Eclipse Public License, the same as Clojure.
+
 (ns dbkid.core)
 
+(defn now
+  "Get current time in format <yyyy-MM-dd HH:mm:ss>."
+  []
+  (.format 
+    (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss")
+    (.getTime (java.util.Calendar/getInstance))))
+
+(def ^:dynamic *debug-flag* (atom true))
+
+(defn db 
+  "Disable/Enable DBK which used to print debug info."
+  [& _] 
+  (reset! *debug-flag* (not @*debug-flag*)) 
+  (println (str "DBK is now " (if @*debug-flag* "Enabled!" "Disabled!"))))
+
+(def ^:dynamic *debug-print-length* (atom 8))
+
+(defn set-print-length [^long l] (reset! *debug-print-length* l))
+
+(defmacro dbk
+  "Print debug info(universal/global debugger)."
+  [& variables]      
+  `(if (deref *debug-flag*)
+    (let [datetime# ~(now)
+          naked-msg# (str "Debug: " datetime# " in " ~*file* "@" ~(:line (meta &form)))]
+      (if (empty? '~variables)
+        (println naked-msg#)
+        (binding [*print-length* (deref *debug-print-length*)]
+           (let [kvs# (into {} (map vec (partition 2 (interleave 
+                                                       (map #(str % " =>") '~variables)
+                                                       (vector ~@variables)))))]
+             (println (str naked-msg# ":") kvs#)))))))
+
+(def ^:dynamic *breakpoint-flag* (atom true))
+
+(defn bp 
+  "Disable/Enable break-points."
+  [& _] 
+  (reset! *breakpoint-flag* (not @*breakpoint-flag*)) 
+  (println (str "Break points are now "
+                (if @*breakpoint-flag* "Enabled!" "Disabled!"))))
+
+(defn- eeval 
+  [expr]
+  (let [tp (type expr)]
+    (cond
+      (= tp clojure.lang.Compiler$NumberExpr) (.eval expr)
+      (= tp clojure.lang.Compiler$DefExpr) (eval (.var expr))
+      (= tp clojure.lang.Compiler$StringExpr) (.eval expr)
+      (= tp clojure.lang.Compiler$InvokeExpr) (let [f (.eval (.fexpr expr))
+                                                    argvs (map #(.eval %) (.args expr))]
+                                                (apply f argvs))
+      (= tp clojure.lang.Compiler$VectorExpr) (.eval expr)
+      (= tp clojure.lang.Compiler$FnExpr) "#local_fn#"
+      (= tp clojure.lang.Compiler$MapExpr) (.eval expr))))
+
+(defmacro ?
+  [& forms]
+  (if (deref *breakpoint-flag*)
+    (binding [*print-length* (deref *debug-print-length*)]
+      (let [prompt #(do (print ">: ") (flush) (read-line))]
+        (loop [cmd (prompt)]
+          (if (or (= cmd "q") (= cmd ""))
+            (rest &form)
+            (do
+              (case cmd
+                "?" (println (str *file* "@" (:line (meta &form))))
+                "l" (println (vec (sort (keys &env))))
+                "ll" (println (vec (sort (keys (ns-interns *ns*)))))
+                (let [s (read-string cmd) lb (if &env (&env s) nil)]
+                  (print (str cmd ": "))
+                  (try
+                    (if lb
+                      (println (eeval (.init lb)))
+                      (println (eval s)))
+                    (catch Exception e
+                      (println (.getMessage e))))))
+              (recur (prompt)))))))
+    (rest &form)))
